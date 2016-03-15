6f1fe5750cb01eb397864d09225ddad12378b3a3
guuse <ekkelenkampguus@gmail.com>
2016-03-15 16:47:14 +0100

    removed line js
    removed line js

--- js/main.js
+++ js/main.js
@@ -3,5 +3,3 @@ function playIt()
     document.getElementById("embed").innerHTML="<embed src='/music/Cartoon_Cowbell.mp3' autostart=true loop=false volume=100 hidden=true>";
     return true;
 }
-
-var snd = new Audio('Music/')
