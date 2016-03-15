c61886c48c29e84cba1fe6b0b3fa5b737f59fb30
guuse <ekkelenkampguus@gmail.com>
2016-03-15 15:22:43 +0100

    12 working buttons
    Added the rest of the buttons

--- index.php
+++ index.php
@@ -21,14 +21,50 @@
 </head>
     <body>
     <div class="row">
-        <div class="col-sm-4" style="text-align: center; margin-top: 3%">
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
             <a href="music/Cartoon_Cowbell.mp3" class="myButton">bell</a>
         </div>
-        <div class="col-sm-4" style="text-align: center; margin-top: 3%">
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
             <a href="music/Cartoon_Boing.mp3" class="myButton">Boing</a>
         </div>
-        <div class="col-sm-4" style="text-align: center; margin-top: 3%">
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
             <a href="music/Clang_and_wobble.mp3" class="myButton">Clang</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Cartoony_Bounce_To_Ceiling.mp3" class="myButton">Bounce</a>
+        </div>
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Hair_Ripping.mp3" class="myButton">Ripping</a>
+        </div>
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Metal_Twang.mp3" class="myButton">Twang</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Tympani_Bing.mp3" class="myButton">Bing</a>
+        </div>
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Wood_Plank_Flicks.mp3" class="myButton">Flicks</a>
+        </div>
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Woodpecker.mp3" class="myButton">Pecker</a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Pop.mp3" class="myButton">Pop</a>
+        </div>
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Metallic_Twang_High.mp3" class="myButton">Twang #2</a>
+        </div>
+        <div class="col-sm-4" style="text-align: center; margin-top: 5%">
+            <a href="music/Metallic_Clank.mp3" class="myButton">Clank</a>
         </div>
     </div>
 </body>
